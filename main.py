88cf0d71cf4456f0a8d6123e8916ed3727632575
Myller Silva <myllersilva1310@gmail.com>
2022-10-22 10:39:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from msilib.schema import Error
-import pandas as pd
-# import pysat.formula 
-from LogiComp.formula import Atom, Or, And
+import pandas as pd 
+from LogiComp.formula import Atom, Implies, Or, And
 
-def list_to_form(or_list = [] , Form=And):
+def list_to_form(or_list = [], Inner=Atom, Form=And):
   n = len(or_list)
+
   if(n==0):
     raise OverflowError('Lista vazia')
   
-  formula =  Atom(or_list[0])
+  formula = Inner(or_list[0])
   for i in range(1, n):
-    formula = Form(formula, or_list[i])
+    formula = Form(formula, Inner(or_list[i]))
   return formula
 
 
@@ -25,24 +24,31 @@ def quinta_restricao(data_frame = pd.DataFrame() , n=3, m=4):
     or_list = list()
     for j in range(0, m): 
       or_list.append(f'C{i+1},{j+1}')
-    print(list_to_form(or_list, Or))
-    and_list.append(or_list)
+    and_list.append( list_to_form(or_list, Atom, Or) )
   return and_list
  
 
+def restricao_dois(data_frame = pd.DataFrame(), n=3, m=4):
+  and_list = list()
+  # AndZao(1 a m )
+  # OrZao
+
+  return
 
 
 
-file_name = 'column_bin_3a_2p.csv'
-data = pd.read_csv(f'./arquivo_dos_pacientes/{file_name}')
+
+# file_name = 'column_bin_3a_2p.csv'
+# data = pd.read_csv(f'./arquivo_dos_pacientes/{file_name}')
+# print(data)
+
+# print("quinta restricao: ")
+# and_list = quinta_restricao(data) 
+
+# print('\n\n\n')
+# print( pd.DataFrame(and_list) ) 
+
+data = pd.read_csv('./exemplo_dado_no_pdf.csv')
 print(data)
 
-print("quinta restricao: ")
-and_list = quinta_restricao(data)
-
-
-print('\n\n\n')
-print( pd.DataFrame(and_list) ) 
-
-
-list_to_form([])
+quinta_restricao(data)
