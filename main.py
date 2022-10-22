6d8e4cfddfd2e94acf94350a47b687c9f72b70a2
Myller Silva <myllersilva1310@gmail.com>
2022-10-22 09:31:49 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+# import pysat.formula 
+
+def quinta_restricao(data_frame = pd.DataFrame() , n=3):
+  #Cada paciente com patologia deve ser coberto por alguma das regras.
+  array = list()
+  
+  
+  return
+
+
+
+
+
+file_name = 'column_bin_3a_2p.csv'
+data = pd.read_csv(f'./arquivo_dos_pacientes/{file_name}')
+print(data)
+
+print("quinta restricao: ")
+quinta_restricao(data)
