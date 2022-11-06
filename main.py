a20b7ccb061bdc437452a4fef03b9631e8458efa
Myller Silva <myllersilva1310@gmail.com>
2022-11-06 18:58:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ from LogiComp.semantics import *
 
 
 
-file = './pacientes_testes/column_bin_2a_3p.csv'
+# file = './pacientes_testes/exemplo_dado_no_pdf.csv'
+file = './arquivo_dos_pacientes/column_bin_3a_3p.csv'
 df = pd.read_csv(f'./{file}')
 m_regras = 2
 
 print(df)
 
-s = solver(df, m_regras)
-if(len(s)!=0):
+set_of_rules = solver(df, m_regras)
+if(len(set_of_rules)!=0):
   print("conjunto de regras: ")
-  for rule in s:
+  for rule in set_of_rules:
     print(rule)
 else:
   print(f'Nao é possivel definir um conjunto de regras com {m_regras} regra(s)')
